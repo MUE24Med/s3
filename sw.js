5b7711edaeb43d575d5148f4be7dbb343b97d9f4
George Reda <dr.george.r.n@gmail.com>
2026-02-12 22:22:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    sw.js - ✅ نسخة مستقرة نهائية
    ======================================== */
 
-const CACHE_NAME = 'semester-3-cache-20260212-8';
+const CACHE_NAME = 'semester-3-cache-20260212-9';
 const urlsToCache = [
     './',
     './index.html',
