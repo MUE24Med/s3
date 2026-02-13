c835437c5bdccfd78a788b4ec39224411abb2ae8
George Reda <dr.george.r.n@gmail.com>
2026-02-13 16:49:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    يحفظ الملفات في المستوى الأعلى (top-level)
    ======================================== */
 
-const CACHE_NAME = 'semester-3-cache-20260214-enhanced';
+const CACHE_NAME = 'semester-3-cache-20260214.1-enhanced';
 
 // ✅ الملفات في المستوى الأعلى (top-level) التي سيتم تخزينها
 const TOP_LEVEL_FILES = [
